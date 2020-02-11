9571e80bd5d1e43bd28bfe6ceecde358984694af
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-11 18:54:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,10 @@ def countdown(number)
     while number != 0
       puts "#{number} SECOND(S)!"
       number -= 1
+      
     end
   end
-returns "HAPPY NEW YEAR!"
+
 
 
 def countdown_with_sleep(number)
