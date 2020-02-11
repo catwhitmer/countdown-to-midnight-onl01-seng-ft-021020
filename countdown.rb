ecebee71a87758fc9f0f226e082186b10ad6741e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-11 18:56:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ def countdown(number)
 
 def countdown_with_sleep(number)
   number = 10
-  while number != 10
+  while number != 0
     puts "#{number} SECOND(S)!"
       number -= 1
       sleep(5.second)
