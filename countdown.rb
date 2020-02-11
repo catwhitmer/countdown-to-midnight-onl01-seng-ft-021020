e22ef03ea3db7e877d9307dde37f3d16b5254d33
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-11 18:38:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   number = 10
     while number >= 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  puts "HAPPY NEW YEAR!"
-end
+      puts "#{number} SECOND(S)!"
+      number -= 1
+      puts "HAPPY NEW YEAR!"
+    end
 end
